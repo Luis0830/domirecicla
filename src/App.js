509aefe0fb39fb846a372738fa455871caf0b347
luis0830 <luiscarlosgarcia0830@gmail.com>
2023-06-24 21:58:03 -0400

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Inicio from "./components/Inicio";
+import Aprende from "./components/Aprende";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import Recicla from "./components/Recicla";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hola</p>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/aprende" element={<Aprende />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/recicla" element={<Recicla />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
