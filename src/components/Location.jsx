c995231d01182356176b52f92c66c88bf9d7d991
luis0830 <luiscarlosgarcia0830@gmail.com>
2023-07-10 01:16:13 -0400

    read crud done

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,31 +1,52 @@
-import React from 'react'
-import { collection, addDoc } from "firebase/firestore"; 
+import React, { useState, useEffect } from 'react'
+import { collection, addDoc, getDocs, updateDoc, deleteDoc, doc } from "firebase/firestore"; 
 import { db } from '../firebase/confi';
 
 
-export default function Location() {
- 
-    const handleAdd = async (e) => {
-    e.preventDefault()
-    try {
-        
-    const res = await addDoc(collection(db, "cities"), {
-        name: "Los Angeles",
-        state: "CA",
-        country: "USA"
-      });
-    } catch (error) {
-      console.log(error)  
-    }
- }
 
+export default function Location() {
+  
+  const [newName, setNewName] = useState("");
+  const [newLat, setNewLat] = useState(0);
+  const [newLog, setNewLog] = useState(0);
+  const [location, setLocation] = useState([]);
+  const locationCollectionRef = collection(db, "location");
+  
+
+  const CreateLocation = async () => {
+     await addDoc(locationCollectionRef, {name: newName, lat: newLat, log: newLog})
+  }
+
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "location", id);
+    await deleteDoc(userDoc);
+  }
+
+  useEffect(() => {
+
+   const getLocation = async () => {
+      const data = await getDocs(locationCollectionRef);
+      setLocation(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+   };
+     getLocation();
+  }, []);
+ 
   return (
-    <div>
-        <form onSubmit={handleAdd}>
-            <h1>agregar nueva sucursal</h1>
-            <input type="text" />
-            <button>agregar</button>
-        </form>
+    <div> 
+      <h1>nueva ubicacion oficina</h1>
+      <input type='string' placeholder='oficina' onChange={(event) => {setNewName(event.target.value)}}/>
+      <input type='number' placeholder='latitud' onChange={(event) => {setNewLat(event.target.value)}}/>
+      <input type='number' placeholder='longitud' onChange={(event) => {setNewLog(event.target.value)}}/>
+      <button onClick={CreateLocation}>agregar</button>
+      {location.map((location) => { 
+        return (
+        <div>
+          <h1>{location.name}</h1>
+          <p>{location.lat}</p>
+          <p>{location.log}</p>
+          <button onClick={ () => {deleteUser(location.id)}}>borrar oficina</button>
+        </div>
+        );})} 
     </div>
   )
 }
